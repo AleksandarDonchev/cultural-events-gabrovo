02eef1695254cff30b7bea802495204011daaad7
Aleksandar Donchev <sa6unko2003@gmail.com>
2026-07-13 15:47:56 +0300

    Fix Municipality file extension case

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"
 
 import Home from "../pages/Home.vue"
-import Municipality from "../pages/Municipality.vue"
+git ls-files client/src/pages
 import Login from "../pages/Login.vue"
 import Register from "../pages/Register.vue"
 
